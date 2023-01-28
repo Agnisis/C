7bfe22add238ac5a891359dccfc5de93c8ac32cb
Agnisis <agnisisdutta16@gmail.com>
2023-01-28 22:59:48 +0530

    initial

---
+++ TOPICS/structure.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+struct student{
+    int roll;
+    float marksTotal;
+    char name[20];
+
+};
+
+void main()
+{
+    struct student s1;
+    s1.name=
+    s1.roll=10;
+    s1.marksTotal=100;
+}
